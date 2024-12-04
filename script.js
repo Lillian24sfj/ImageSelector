42cc8a663d1cd7a4a393e8386030bc529398e127
Lillian24sfj <lillian24sfj@gmail.com>
2024-12-04 14:18:02 +0100

    endringer

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function changeImage() {
   const selectedOption = imageSelect.value;
 
   if (selectedOption === "image1") {
-    selectedImage.src = "/søt hund.jpg";
+    selectedImage.src = "./søt hund.jpg";
     selectedImage.style.display = "block";
   } else if (selectedOption === "image2") {
-    selectedImage.src = "/søt hund2.jpg";
+    selectedImage.src = "./søt hund2.jpg";
     selectedImage.style.display = "block";
   } else if (selectedOption === "image3") {
-    selectedImage.src = "/søt hund 3.jpg";
+    selectedImage.src = "./søt hund 3.jpg";
     selectedImage.style.display = "block";
   } else if (selectedOption === "image4") {
-    selectedImage.src = "/søt hund 4.jpg";
+    selectedImage.src = "./søt hund 4.jpg";
     selectedImage.style.display = "block";
   } else if (selectedOption === "image5") {
-    selectedImage.src = "/søt hund 5.jpg";
+    selectedImage.src = "./søt hund 5.jpg";
     selectedImage.style.display = "block";
   } else {
     selectedImage.style.display = "none";
